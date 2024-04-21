075179289715d1e58a13921b5a3eb26005754d75
efe <113731512+efenatuyo@users.noreply.github.com>
2024-04-21 12:00:47 +0200

    Update database.py

--- src/database.py
+++ src/database.py
@@ -27,16 +27,17 @@ async def receive(self, response):
         }
 
         if event_type in handler_mapping:
-            await handler_mapping[event_type](self, event_data)
-
+            return await handler_mapping[event_type](self, event_data, event_type)
+        
+        print(response)
+        
 def get_or_create_key(dictionary, key, default=None):
     if key not in dictionary:
         dictionary[key] = default if default is not None else {}
     return dictionary[key]
 
-async def handle_user_event(self, data):
+async def handle_user_event(self, data, event_name):
     user_id = data.get("member", {}).get("user", {}).get("id") or data.get("author", {}).get("id")
-    event_name = data.get("_event_name")
 
     user_data = get_or_create_key(self.data, "users", {})
     user_event_data = get_or_create_key(user_data, user_id, {})
@@ -46,9 +47,8 @@ async def handle_user_event(self, data):
     with open("data.json", "w") as file:
         json.dump(self.data, file, indent=4)
 
-async def handle_guild_event(self, data):
+async def handle_guild_event(self, data, event_name):
     guild_id = data.get("guild_id") or data.get("id")
-    event_name = data.get("_event_name")
 
     guild_data = get_or_create_key(self.data, "guilds", {})
     guild_event_data = get_or_create_key(guild_data, guild_id, {})
