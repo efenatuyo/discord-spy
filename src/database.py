190a0d8dd07422e728f3217514c666f96e6c4f42
efe <113731512+efenatuyo@users.noreply.github.com>
2024-04-21 11:55:55 +0200

    Update database.py

--- src/database.py
+++ src/database.py
@@ -5,196 +5,55 @@ async def receive(self, response):
         event_type = response.get("t")
         event_data = response.get("d")
 
-        if event_type in ["GUILD_CREATE", "GUILD_UPDATE", "MESSAGE_CREATE", "TYPING_START",
-                          "PRESENCE_UPDATE", "MESSAGE_UPDATE", "MESSAGE_REACTION_ADD",
-                          "GUILD_ROLE_UPDATE", "GUILD_ROLE_CREATE", "GUILD_ROLE_DELETE",
-                          "MESSAGE_DELETE", "MESSAGE_REACTION_REMOVE", "GUILD_BAN_ADD",
-                          "MESSAGE_DELETE_BULK", "GUILD_BAN_REMOVE", "CONVERSATION_SUMMARY_UPDATE"]:
-            handler_function = globals().get(f"{event_type.lower()}")
-            if handler_function:
-                return await handler_function(self, event_data)
-    # any unhandled event probally can ignore them
-    
-    print(response)
+        handler_mapping = {
+            "GUILD_CREATE": handle_guild_event,
+            "GUILD_UPDATE": handle_guild_event,
+            "MESSAGE_CREATE": handle_user_event,
+            "TYPING_START": handle_user_event,
+            "PRESENCE_UPDATE": handle_user_event,
+            "MESSAGE_UPDATE": handle_user_event,
+            "MESSAGE_REACTION_ADD": handle_user_event,
+            "GUILD_ROLE_UPDATE": handle_guild_event,
+            "GUILD_ROLE_CREATE": handle_guild_event,
+            "GUILD_ROLE_DELETE": handle_guild_event,
+            "MESSAGE_DELETE": handle_user_event,
+            "MESSAGE_REACTION_REMOVE": handle_user_event,
+            "GUILD_BAN_ADD": handle_guild_event,
+            "MESSAGE_DELETE_BULK": handle_user_event,
+            "GUILD_BAN_REMOVE": handle_guild_event,
+            "CONVERSATION_SUMMARY_UPDATE": handle_guild_event,
+            "CHANNEL_UPDATE": handle_guild_event,
+            "GUILD_MEMBER_REMOVE": handle_guild_event
+        }
 
-async def message_create(self, data):
-    user = data["id"]
-    if not self.data.get("users"):
-        self.data["users"] = {user: {"MESSAGE_CREATE": data}}
-    elif not self.data["users"].get(user) or not self.data["users"][user].get("MESSAGE_CREATE"):
-        self.data["users"][user] = {"MESSAGE_CREATE": [data]}
-    else:
-        if not data in self.data["users"][user]["MESSAGE_CREATE"]:
-            self.data["users"][user]["MESSAGE_CREATE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
+        if event_type in handler_mapping:
+            await handler_mapping[event_type](self, event_data)
 
-async def guild_role_update(self, data):
-    guild = data["guild_id"]
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild: {"GUILD_ROLE_UPDATE": data}}
-    elif not self.data["guilds"].get(guild) or not self.data["guilds"][guild].get("GUILD_ROLE_UPDATE"):
-        self.data["guilds"][guild] = {"GUILD_ROLE_UPDATE": [data]}
-    else:
-        if not data in self.data["guilds"][guild]["GUILD_ROLE_UPDATE"]:
-            self.data["guilds"][guild]["GUILD_ROLE_UPDATE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
+def get_or_create_key(dictionary, key, default=None):
+    if key not in dictionary:
+        dictionary[key] = default if default is not None else {}
+    return dictionary[key]
 
-async def conversation_summary_update(self, data):
-    guild = data["guild_id"]
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild: {"CONVERSATION_SUMMARY_UPDATE": data}}
-    elif not self.data["guilds"].get(guild) or not self.data["guilds"][guild].get("CONVERSATION_SUMMARY_UPDATE"):
-        self.data["guilds"][guild] = {"CONVERSATION_SUMMARY_UPDATE": [data]}
-    else:
-        if not data in self.data["guilds"][guild]["CONVERSATION_SUMMARY_UPDATE"]:
-            self.data["guilds"][guild]["CONVERSATION_SUMMARY_UPDATE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
-    
-async def guild_role_delete(self, data):
-    guild = data["guild_id"]
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild: {"GUILD_ROLE_DELETE": data}}
-    elif not self.data["guilds"].get(guild) or not self.data["guilds"][guild].get("GUILD_ROLE_DELETE"):
-        self.data["guilds"][guild] = {"GUILD_ROLE_DELETE": [data]}
-    else:
-        if not data in self.data["guilds"][guild]["GUILD_ROLE_DELETE"]:
-            self.data["guilds"][guild]["GUILD_ROLE_DELETE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
+async def handle_user_event(self, data):
+    user_id = data.get("member", {}).get("user", {}).get("id") or data.get("author", {}).get("id")
+    event_name = data.get("_event_name")
 
-async def message_reaction_remove(self, data):
-    guild = data["guild_id"]
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild: {"MESSAGE_REACTION_REMOVE": data}}
-    elif not self.data["guilds"].get(guild) or not self.data["guilds"][guild].get("MESSAGE_REACTION_REMOVE"):
-        self.data["guilds"][guild] = {"MESSAGE_REACTION_REMOVE": [data]}
-    else:
-        if not data in self.data["guilds"][guild]["MESSAGE_REACTION_REMOVE"]:
-            self.data["guilds"][guild]["MESSAGE_REACTION_REMOVE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
+    user_data = get_or_create_key(self.data, "users", {})
+    user_event_data = get_or_create_key(user_data, user_id, {})
+    user_event_data[event_name] = user_event_data.get(event_name, [])
+    user_event_data[event_name].append(data)
 
-async def guild_ban_revoke(self, data):
-    guild = data["guild_id"]
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild: {"GUILD_BAN_REMOVE": data}}
-    elif not self.data["guilds"].get(guild) or not self.data["guilds"][guild].get("GUILD_BAN_REMOVE"):
-        self.data["guilds"][guild] = {"GUILD_BAN_REMOVE": [data]}
-    else:
-        if not data in self.data["guilds"][guild]["GUILD_BAN_REMOVE"]:
-            self.data["guilds"][guild]["GUILD_BAN_REMOVE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
+    with open("data.json", "w") as file:
+        json.dump(self.data, file, indent=4)
 
-async def guild_ban_add(self, data):
-    guild = data["guild_id"]
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild: {"GUILD_BAN_ADD": data}}
-    elif not self.data["guilds"].get(guild) or not self.data["guilds"][guild].get("GUILD_BAN_ADD"):
-        self.data["guilds"][guild] = {"GUILD_BAN_ADD": [data]}
-    else:
-        if not data in self.data["guilds"][guild]["GUILD_BAN_ADD"]:
-            self.data["guilds"][guild]["GUILD_BAN_ADD"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
+async def handle_guild_event(self, data):
+    guild_id = data.get("guild_id") or data.get("id")
+    event_name = data.get("_event_name")
 
+    guild_data = get_or_create_key(self.data, "guilds", {})
+    guild_event_data = get_or_create_key(guild_data, guild_id, {})
+    guild_event_data[event_name] = guild_event_data.get(event_name, [])
+    guild_event_data[event_name].append(data)
 
-async def message_delete(self, data):
-    guild = data["guild_id"]
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild: {"MESSAGE_DELETE": data}}
-    elif not self.data["guilds"].get(guild) or not self.data["guilds"][guild].get("MESSAGE_DELETE"):
-        self.data["guilds"][guild] = {"MESSAGE_DELETE": [data]}
-    else:
-        if not data in self.data["guilds"][guild]["MESSAGE_DELETE"]:
-            self.data["guilds"][guild]["MESSAGE_DELETE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
-
-async def message_delete_bulk(self, data):
-    guild = data["guild_id"]
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild: {"MESSAGE_DELETE_BULK": data}}
-    elif not self.data["guilds"].get(guild) or not self.data["guilds"][guild].get("MESSAGE_DELETE_BULK"):
-        self.data["guilds"][guild] = {"MESSAGE_DELETE_BULK": [data]}
-    else:
-        if not data in self.data["guilds"][guild]["MESSAGE_DELETE_BULK"]:
-            self.data["guilds"][guild]["MESSAGE_DELETE_BULK"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
-    
-async def guild_role_create(self, data):
-    guild = data["guild_id"]
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild: {"GUILD_ROLE_CREATE": data}}
-    elif not self.data["guilds"].get(guild) or not self.data["guilds"][guild].get("GUILD_ROLE_CREATE"):
-        self.data["guilds"][guild] = {"GUILD_ROLE_CREATE": [data]}
-    else:
-        if not data in self.data["guilds"][guild]["GUILD_ROLE_CREATE"]:
-            self.data["guilds"][guild]["GUILD_ROLE_CREATE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
-    
-async def message_reaction_add(self, data):
-    user = data["member"]["user"]["id"]
-    if not self.data.get("users"):
-        self.data["users"] = {user: {"MESSAGE_REACTION_ADD": data}}
-    elif not self.data["users"].get(user) or not self.data["users"][user].get("MESSAGE_REACTION_ADD"):
-        self.data["users"][user] = {"MESSAGE_REACTION_ADD": [data]}
-    else:
-        if not data in self.data["users"][user]["MESSAGE_REACTION_ADD"]:
-            self.data["users"][user]["MESSAGE_REACTION_ADD"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
-
-async def message_update(self, data):
-    user = data["author"]["id"]
-    if not self.data.get("users"):
-        self.data["users"] = {user: {"MESSAGE_UPDATE": data}}
-    elif not self.data["users"].get(user) or not self.data["users"][user].get("MESSAGE_UPDATE"):
-        self.data["users"][user] = {"MESSAGE_UPDATE": [data]}
-    else:
-        if not data in self.data["users"][user]["MESSAGE_UPDATE"]:
-            self.data["users"][user]["MESSAGE_UPDATE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
-    
-async def typing_start(self, data):
-    user = data["member"]["user"]["id"]
-    if not self.data.get("users"):
-        self.data["users"] = {user: {"TYPING_START": data}}
-    elif not self.data["users"].get(user) or not self.data["users"][user].get("TYPING_START"):
-        self.data["users"][user] = {"TYPING_START": [data]}
-    else:
-        if not data in self.data["users"][user]["TYPING_START"]:
-            self.data["users"][user]["TYPING_START"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
-
-async def presence_update(self, data):
-    user = data["user"]["id"]
-    if not self.data.get("users"):
-        self.data["users"] = {user: {"PRESENCE_UPDATE": data}}
-    elif not self.data["users"].get(user) or not self.data["users"][user].get("PRESENCE_UPDATE"):
-        self.data["users"][user] = {"PRESENCE_UPDATE": [data]}
-    else:
-        if not data in self.data["users"][user]["PRESENCE_UPDATE"]:
-            self.data["users"][user]["PRESENCE_UPDATE"].append(data)
-        
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
-
-        
-async def guild_create(self, data, static=False):
-    guild_id = str(data["guild_id" if not static else "id"])
-    if not self.data.get("guilds"):
-        self.data["guilds"] = {guild_id: {"GUILD_CREATE": data}}
-    else:
-        if not self.data["guilds"].get(guild_id):
-            self.data["guilds"][guild_id] = {"GUILD_CREATE": data}
-        else:
-            self.data["guilds"][guild_id]["GUILD_CREATE"] = data
-    
-    open("data.json", "w").write(json.dumps(self.data, indent=4))
+    with open("data.json", "w") as file:
+        json.dump(self.data, file, indent=4)
